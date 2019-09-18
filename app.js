bd6e923f089ecf9fd207ba3e36179503844f5e6f
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2019-09-18 12:31:08 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 let loaf = {
     flour: 300,
     water: 210,
@@ -55,17 +56,49 @@ function decodeWords(encodedArr) {
 }
 console.log(decodeWords(test));
 
-function createCharacter() {
+
+
+function createCharacter(name, nickname, race, origin, attack ,defence) {
     return {
-        name: '',
-        nickname: '',
-        race: '',
-        origin: '',
+        name,
+        nickname,
+        race,
+        origin,
+        attack,
+        defence,
         describe: function () {
-           console.log(`${this.name} is a ${this.race} from ${this.origin}.`)
-        },
+            if(this.equipped) {
+                console.log(`${this.name} is a ${this.race} from ${this.origin} who uses ${this.equipped}.`)
+            } else {
+                console.log(`${this.name} is a ${this.race} from ${this.origin}.`)
+            }
+            },
         evaluateFight: function(character) {
-            "Your opponent takes {x} damage and you receive {y} damage"
+            console.log(`Your opponent takes ${(character.defence > this.attack) ? 0 : this.attack - character.defence} damage and you receive ${(this.defence > character.attack) ? 0 : character.attack - this.defence} damage`);
         }
     }
 }
+let characters  = [createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6),
+    createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1),
+    createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
+    createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8),
+    createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5),
+];
+characters.push(createCharacter('Arwen Undomiel', 'arwen', 'Half-Elf', 'Rivendell', 4,3));
+characters.find((char) => char.nickname === 'aragorn').describe()
+let hobbits = characters.filter((char) => char.race === 'Hobbit');
+console.log(hobbits);
+let strChars = characters.filter((char) => char.attack > 5);
+console.log(strChars);
+
+function equipItem(character, string) {Object.assign(character, {equipped: string})}
+equipItem(characters[0], 'a wizard staff');
+equipItem(characters[1], 'the Ring');
+equipItem(characters[2], 'String and Barrow Blade');
+equipItem(characters[2], 'String and Barrow Blade');
+equipItem(characters[3], 'Anduril');
+equipItem(characters[4], 'Bow and Arrow');
+equipItem(characters[5], 'Hadhafang');
+
+characters [1].evaluateFight(characters[0]);
+characters.forEach(character => character.describe());
